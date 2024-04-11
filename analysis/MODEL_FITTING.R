d2a6c8af7d73f6cda97e56352cc810dad8644b4d
damneet <damneetthiara@gmail.com>
2024-04-11 12:22:01 -0700

    Update MODEL_FITTING.R

--- analysis/MODEL_FITTING.R
+++ analysis/MODEL_FITTING.R
@@ -7,7 +7,7 @@ N<-29
 fit = stan(
   seed = 123,
   file = "~/Desktop/MODEL1.stan",  
-  data = list(u=u,N=N),      
+  data = list(u=u,v=v,N=N),      
   iter = 1000                   
 )
 
@@ -16,4 +16,9 @@ fit
 samples = extract(fit)$mu2
 n_samples = nrow(samples)
 
-plot(1:N,samples[i,])
+plot(v,mu2_vector)
+plot(v,u)
+mu2_vector <- c(0.09, 0.10, 0.09, 0.07, 0.08, 0.08, 0.08, 0.07, 0.07, 0.07,
+                0.06, 0.06, 0.06, 0.07, 0.06, 0.05, 0.05, 0.06, 0.05, 0.05,
+                0.05, 0.05, 0.06, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05)
+
