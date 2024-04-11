c58ff83058382fea59dd409f700de4b9230d52ff
damneet <damneetthiara@gmail.com>
2024-04-11 10:30:38 -0700

    Update MODEL_FITTING.R

--- analysis/MODEL_FITTING.R
+++ analysis/MODEL_FITTING.R
@@ -1,15 +1,14 @@
 require(rstan)
-library(rstan)
-Yes
 u<-ur_data$UR/100
 v<-Canadian_vaccines$dose_rate/100
 N<-29
 
+
 fit = stan(
   seed = 123,
-  file = "~/Desktop/MODEL1.stan",  # Stan program
-  data = list(u=u,v=v,N=N),        # named list of data
-  iter = 1000                   # number of samples to draw
+  file = "~/Desktop/MODEL1.stan",  
+  data = list(u=u,N=N),      
+  iter = 1000                   
 )
 
 fit
@@ -17,14 +16,7 @@ fit
 samples = extract(fit)$mu
 n_samples = nrow(samples)
 
-plot(v, u)
 
-for (i in 1:n_samples) {
-  lines(v, samples[i,])
-}
-
-plot(samples[i,])
-
-
-
+plot(1:N,samples[i,])
+plot(1:N,u)
 
